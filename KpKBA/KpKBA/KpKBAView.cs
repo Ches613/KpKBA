4df547db26fe6bc8839936c069bd30175cfb332f
ches613 <zverkovmi@gmail.com>
2017-04-09 22:33:12 +0300

    Fix1

---
+++ KpKBA/KpKBA/KpKBAView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scada.Comm.Devices
+{
+    public sealed class KpKBAView : KPView
+    {
+        public override string KPDescr
+        {
+            get
+            {
+                return "Библиотека КП для KBA лазера.";
+            }
+        }
+    }
+}
