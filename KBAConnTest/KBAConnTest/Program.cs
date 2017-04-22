3ce8b98eae6bd790becfb78a89df33c2fe6e7404
ches613 <zverkovmi@gmail.com>
2017-04-22 13:14:23 +0300

    Консольное приложение для тестирования класса Laser.

---
+++ KBAConnTest/KBAConnTest/Program.cs
@@ -0,0 +1,41 @@
+﻿using Scada.Comm.Devices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KBAConnTest
+{
+    class Program
+    {
+
+        private static Laser laser;
+        private static StatusPack laserStat;
+
+
+        static void Main(string[] args)
+        {
+
+             laser = new Laser("10.7.66.127", 3490);
+
+             laserStat = new StatusPack();
+
+            while (true) {
+
+
+               
+                   laserStat = laser.getStatus();
+
+                   Console.WriteLine("UM = " + laser.reqActualNum(1));
+                    Console.WriteLine("okPrintCount = " + laserStat.okPrintCount);
+
+                Console.ReadKey();
+               // Thread.Sleep(1000);
+
+            }
+
+        }
+    }
+}
